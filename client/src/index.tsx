ecc79c1fd3058736536f03c48ca17f9c176598e7
Gabriel David <gabrielcandez@gmail.com>
2020-07-06 14:47:50 -0300

    Initial setup

---
+++ client/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, } from 'react-dom';
+
+const App = () => (
+    <h1>
+        <div>
+            asd
+        </div>
+    </h1>
+)
+
+const mountingNode = document.getElementById('root',);
+render(<App />, mountingNode,);
